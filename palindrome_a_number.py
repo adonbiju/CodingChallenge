14d9bf8c8b940053b1ae16b0398b32576e051cce
adonbiju <adonbiju@gmail.com>
2022-07-27 19:07:11 +0530

    palindrome a number using python

--- palindrome_a_number.py
+++ palindrome_a_number.py
@@ -2,8 +2,8 @@ num=int(input("enter a number"))
 temp=num
 rev=0
 while num>0:
-    digit=num%10
-    rev=rev*10+digit
+    rem=num%10
+    rev=rev*10+rem
     num=num//10
 if temp==rev:
     print("The number is a palindrome!")
