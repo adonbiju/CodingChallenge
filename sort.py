b4717956939da38c11609030196efdf16ab6a867
adonbiju <adonbiju@gmail.com>
2022-07-28 20:37:19 +0530

    sort a list using python

---
+++ sort.py
@@ -0,0 +1,21 @@
+a = ["b", "g", "a", "d", "f", "c", "h", "e"]
+#x = sorted(a)
+#print(x)
+
+# Sorting a list in Ascending Order
+a.sort()
+
+# Sorting a list in Descending Order
+#a.sort(reverse=True)
+
+print(a)
+
+
+b = [10 , 9, 13, 6 , 17]
+# Sorting a list in Ascending Order
+y=b.sort()
+
+# Sorting a list in Descending Order
+#y=b.sort(reverse=True)
+
+print(b)
