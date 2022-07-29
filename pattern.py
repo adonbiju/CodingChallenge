46f53b1e127d98e98f966f780bc20c3ad79cc9ce
adonbiju <adonbiju@gmail.com>
2022-07-29 19:20:15 +0530

    print diffrent  using python

---
+++ pattern.py
@@ -0,0 +1,142 @@
+# limit=int(input("enter the limit:"))
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print("\n")
+
+# 1
+#
+# 22
+#
+# 333
+#
+# 4444
+#
+# 55555
+
+
+
+# limit=int(input("enter the limit:"))
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print("\n")
+
+# 1
+#
+# 12
+#
+# 123
+#
+# 1234
+#
+# 12345
+
+
+# limit=int(input("enter the limit:"))
+# for i in range(limit,0,-1):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print("\n")
+# 333
+#
+# 22
+#
+# 1
+
+
+
+# limit=int(input("enter the limit:"))
+# for i in range(limit,0,-1):
+#     for j in range(1,i+1):
+#         print(j,end="")
+#     print("\n")
+# 123
+#
+# 12
+#
+# 1
+
+
+# limit=int(input("enter the limit:"))
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print("\n")
+
+# *
+#
+# **
+#
+# ***
+#
+# ****
+#
+# *****
+
+
+limit=int(input("enter the limit:"))
+for i in range(limit,0,-1):
+    for j in range(1,i+1):
+        print("*",end="")
+    print("\n")
+# ****
+#
+# ***
+#
+# **
+#
+# *
+
+
+
+# limit=int(input("enter the limit:"))
+# num=1
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#         print(num,end="")
+#         num=num+1
+#     print("\n")
+
+# 1
+#
+# 23
+#
+# 456
+
+
+
+# limit=int(input("enter the limit:"))
+# num=65
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#         ch=chr(num)
+#         print(ch,end="")
+#         num=num+1
+#     print("\n")
+
+# A
+#
+# BC
+#
+# DEF
+#
+# GHIJ
+#
+# KLMNO
+
+
+# limit=int(input("enter the limit:"))
+# num=65
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#         ch=chr(num)
+#         print(ch,end="")
+#     num = num + 1
+#     print("\n")
+
+# A
+#
+# BB
+#
+# CCC
