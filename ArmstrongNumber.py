9013ec937573390ef77070fe2b2928ce6fe94a39
adonbiju <adonbiju@gmail.com>
2022-08-25 19:33:43 +0530

    Armstong number using python

---
+++ ArmstrongNumber.py
@@ -0,0 +1,21 @@
+# Python program to check if the number is an Armstrong number or not for 3 digit
+
+# take input from the user
+num = int(input("Enter a number: "))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while num > 0:
+   digit = num % 10
+   #print(digit)
+   sum += digit ** 3
+   num= num// 10
+
+# display the result
+if temp == sum:
+   print(temp,"is an Armstrong number")
+else:
+   print(temp,"is not an Armstrong number")
